10ac045e4491a3af20767afe82d54246b5ffc0f9
Frank + Patrick <patrick.geschinski@hdi.de>
2019-06-27 11:12:53 +0200

    Moved Filter Config

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,7 +1,15 @@
 package org.superbiz.struts;
 
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +17,33 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public FilterRegistrationBean siteMeshFilter(FilterRegistrationBean bean) {
+        bean.setFilter(new SiteMeshFilter());
+        bean.setUrlPatterns(Arrays.asList(
+                "/*"
+        ));
+        Map<String, String> params = new HashMap<>();
+        params.put("actionPackages", "com.lq");
+        bean.setInitParameters(params);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean strutsPrepareAndExecuteFilter(FilterRegistrationBean bean) {
+        StrutsPrepareAndExecuteFilter filter = new StrutsPrepareAndExecuteFilter();
+        bean.setFilter(filter);
+        bean.setUrlPatterns(Arrays.asList(
+                "/",
+                "/addUserForm.action",
+                "/addUser.action",
+                "/findUserForm.action",
+                "/findUser.action",
+                "/listAllUsers.action"
+                ));
+        return bean;
+
+    }
+
 }
